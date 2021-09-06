628ac05b8522f51050a840fae40f8baab94a4abb
Oruj Ahmadov <oruj.ahmadov@accenture.com>
2021-09-06 03:09:24 -0400

    move download button and cleanup

--- content.js
+++ content.js
@@ -1,11 +1,8 @@
 const DOWNLOAD_BUTTON_ID = 'extension-button-attached';
 const REGEX_URL = '500px.com\/photo\/*';
 
-const getRoot = () => {
-  return document.getElementById('pxLightbox-1') || document.getElementById('root');
-}
-
-const getImageTitle = (root) => {
+function getImageTitle() {
+  const root = document.getElementById('pxLightbox-1') || document.getElementById('root');
   let imageTitle = null;
   /* Try to get title of photo */
   const h3Elements = root.getElementsByTagName('H3');
@@ -16,23 +13,19 @@ const getImageTitle = (root) => {
   return imageTitle || '500px';
 }
 
-const attachDownloadButton = () => {
-  const root = getRoot();
+function attachDownloadButton(parent) {
 
-  if (root) {
+  if (parent) {
     const button = document.createElement("BUTTON");
     const text = document.createTextNode(chrome.i18n.getMessage("download_button"));
     button.appendChild(text);
     button.id = DOWNLOAD_BUTTON_ID;
-    button.style.position = 'absolute';
-    button.style.top = '65%';
-    button.style.right = '25px';
     button.style.border = '0px';
-    button.style.padding = '15px';
+    button.style.padding = '12px';
     button.style.borderRadius = '5px';
     button.style.color = 'white';
     button.style.cursor = 'pointer';
-    button.style.backgroundColor = 'black';
+    button.style.backgroundColor = 'deepskyblue';
     button.style.zIndex = 100;
     button.title = 'Added by 500px Downloader Extension';
     button.addEventListener("focus", function () {
@@ -40,25 +33,43 @@ const attachDownloadButton = () => {
     });
     button.addEventListener('click', () => {
       const imageAddress = document.querySelector('.photo-show__img').src;
-      chrome.runtime.sendMessage({"message": "download_image", "url": imageAddress, "title": getImageTitle(root)});
+      chrome.runtime.sendMessage({"message": "download_image", "url": imageAddress, "title": getImageTitle()});
     });
-    root.appendChild(button);
+    parent.appendChild(button);
   }
 }
 
+function findChild(node) {
+  if (node.className && typeof node.className === "string" && node.className.includes("Elements__PhotoButton")) {
+    return node;
+  }
+  if (node.childNodes.length > 0) {
+    for (let child of node.childNodes) {
+      const foundNode = findChild(child);
+      if (foundNode) {
+        return foundNode;
+      }
+    }
+  }
+
+  return undefined;
+}
+
 // Callback function to execute when mutations are observed
-const callback = function(_, _) {
+function mutationCallback(_, _) {
   const downloadButton = document.getElementById(DOWNLOAD_BUTTON_ID);
+  const container = document.getElementById("copyrightTooltipContainer");
+  
   const matches = window.location.href.match(REGEX_URL);
-  if (matches && !downloadButton) {
-    attachDownloadButton();
+  if (matches && !downloadButton && container) {
+    attachDownloadButton(findChild(container.nextSibling).parentNode);
   } else if (!matches && downloadButton) {
     downloadButton.remove();
   }
 };
 
 // Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+const observer = new MutationObserver(mutationCallback);
 // Select the node that will be observed for mutations
 const targetNode = document.getElementsByTagName('body')[0];
 // Options for the observer (which mutations to observe)
@@ -68,11 +79,10 @@ const config = { attributes: true, childList: true, subtree: true };
 observer.observe(targetNode, config);
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  function(request, _, _) {
     if( request.message === "clicked_browser_action" ) {
-      const root = getRoot();
       const imageAddress = document.querySelector('.photo-show__img').src;
-      chrome.runtime.sendMessage({"message": "download_image", "url": imageAddress, "title": getImageTitle(root)});
+      chrome.runtime.sendMessage({"message": "download_image", "url": imageAddress, "title": getImageTitle()});
     }
   }
 );
